1753654c4a8074f6fbb3f95e6569741f4d2c3c1c
OdenTakashi <odentakashi@icloud.com>
2022-02-22 00:27:46 +0900

    途中経過
    ファイル内の行数、ワード数、およびバイト数、ファイル名出力実装

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -13,5 +13,40 @@
 		# - ワード数の取得
 		#	method a3
 		# - およびバイト数の取得
+		
+# ディレクトリを指定した場合
+# wc: ../: read: Is a directory
+	#  0       0       0 total
 
-        
+def output(str, f)
+  get_file_lines(str)
+  get_words_count(str)
+	get_bytes_values(f)
+	puts " #{f}"
+end
+
+def get_file_lines(str)
+	print "       "
+	print str.count("\n")
+end
+
+def get_words_count(str)
+	ary = str.split(/\s+/)
+	print "       "
+	print ary.size
+end
+
+def get_bytes_values(f)
+	print "      "
+	print File.size(f)
+end
+
+def main
+	filesnames = ARGV
+	filesnames.each do |f|
+		str = File.read(f)
+		output(str, f)
+	end
+end
+
+main
