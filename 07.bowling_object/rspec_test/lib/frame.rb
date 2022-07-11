3e43584a6ca62f10030ba7ca0c09a866b03611d1
OdenTakashi <odentakashi@icloud.com>
2022-07-11 21:51:11 +0900

    frameclassのscoreメソッドのテストを作成

---
+++ 07.bowling_object/rspec_test/lib/frame.rb
@@ -0,0 +1,13 @@
+class Frame
+	attr_reader :first_shot, :second_shot, :third_shot
+
+	def initialize(first_shot, second_shot, third_shot = nil)
+		@first_shot = Shot.new(first_shot)
+		@second_shot = Shot.new(second_shot)
+		@third_shot = Shot.new(third_shot)
+	end
+
+	def score
+		@first_shot.score + @second_shot.score + @third_shot.score
+	end
+end
---
+++ 07.bowling_object/rspec_test/spec/frame_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+RSpec.describe Frame do 
+	it "return_frame_score" do
+		frame = Frame.new(1, 9)
+		expect(frame.score).to eq 10
+	end
+end
