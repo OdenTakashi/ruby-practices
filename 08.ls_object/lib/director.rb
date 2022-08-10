9fbe9068776dfec18c2cc86c3fc5d93e88abf1e2
OdenTakashi <odentakashi@icloud.com>
2022-08-10 12:49:59 +0900

    不要なファイルを削除

--- 08.ls_object/lib/director.rb
+++ 08.ls_object/lib/director.rb
@@ -11,24 +11,18 @@ class Director
     @director = ListBuilder.new(argv, flags)
   end
 
-  # メソッドを変数に入れていない点が違和感
   def work
     @director.adjust_number_of_iles
     grouped_files = @director.group_files
     @director.result(grouped_files)
   end
 
-  def work_with_l_option
-    
-  end
+  def work_with_l_option; end
 
   def result
     @director.reverse_files if @options['r']
     @options['l'] ? work_with_l_option : work
   end
 end
-
-# オプションの処理はディレクタークラスがやるべき
-# なのでオプション処理はメソッド内でやるべき？
 
 Director.new(ARGV).result
