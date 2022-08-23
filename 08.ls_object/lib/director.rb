a0c16b247fa3bc82ed3e0b1b7063b49f8f8e9268
OdenTakashi <odentakashi@icloud.com>
2022-08-23 16:06:39 +0900

    インスタンス変数の名前の変更

--- 08.ls_object/lib/director.rb
+++ 08.ls_object/lib/director.rb
@@ -7,21 +7,21 @@ class Director
   def initialize(argv)
     @options = argv.getopts('arl')
     flags = @options['a'] ? File::FNM_DOTMATCH : 0
-    @director = ListBuilder.new(argv, flags)
+    @builder = ListBuilder.new(argv, flags)
   end
 
   def work
-    @director.adjust_number_of_iles
-    grouped_files = @director.group_files
-    @director.result(grouped_files)
+    @builder.adjust_number_of_iles
+    grouped_files = @builder.group_files
+    @builder.result(grouped_files)
   end
 
   def work_with_l_option
-    @director.result_with_l_option
+    @builder.result_with_l_option
   end
 
   def result
-    @director.reverse_files if @options['r']
+    @builder.reverse_files if @options['r']
     @options['l'] ? work_with_l_option : work
   end
 end
