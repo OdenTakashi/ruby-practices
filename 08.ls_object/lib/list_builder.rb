06f5925e653a00edbb1aab6abd64cc671dd61b1b
OdenTakashi <odentakashi@icloud.com>
2022-08-24 17:01:33 +0900

    rubocopを通した

--- 08.ls_object/lib/list_builder.rb
+++ 08.ls_object/lib/list_builder.rb
@@ -55,11 +55,11 @@ class ListBuilder
 
   def conversion_permission(permission_octal)
     overhaul_permission = permission_octal.to_i.digits.reverse
-    #[-3..]によって権限の値のみ操作
+    # [-3..]によって権限の値のみ操作
     permission_conversioned = overhaul_permission[-3..].map do |number|
       PERMISSION_TABLE[number]
     end
-    #[0..1]によってファイルかディレクトリを判定
+    # [0..1]によってファイルかディレクトリを判定
     case overhaul_permission[0..1].join
     when '10'
       permission_conversioned.prepend('--')
