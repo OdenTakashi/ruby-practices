cf5144f8f666fe7296fb6762a2783712d648d4bf
OdenTakashi <odentakashi@icloud.com>
2022-09-06 11:51:02 +0900

    lオプション適用時にズレがあったので修正

--- 08.ls_object/lib/list_builder.rb
+++ 08.ls_object/lib/list_builder.rb
@@ -36,18 +36,67 @@ class ListBuilder
     end
   end
 
-  def result_with_l_option
-    @list.files.each do |file|
-      user_id = Process.uid
-      user_name = Etc.getpwuid(user_id).name
-      group_id   = Process.gid
-      group_name = Etc.getgrgid(group_id).name
+  def get_long_formats
+    @list.files.each_with_object([]) do |file, long_formats|
       file_path = File.expand_path(file, @list.directory_name)
       stat = File::Stat.new(file_path)
       permission_octal = stat.mode.to_s(8)
-      permission = conversion_permission(permission_octal)
 
-      puts "#{permission} #{stat.nlink} #{user_name} #{group_name}  #{File.size(file_path)} #{stat.mtime.to_s.slice!(6..15)} #{file} "
+      long_format = {
+        permission: conversion_permission(permission_octal),
+        links: stat.nlink.to_s,
+        user_name: Etc.getpwuid(stat.uid).name,
+        group_name: Etc.getgrgid(stat.gid).name,
+        file_size: File.size(file_path).to_s,
+        last_update_time: get_mtime(stat),
+        file_name: file
+      }
+
+      long_formats << long_format
+    end
+  end
+
+  def get_max_length(_formats)
+    long_formats = get_long_formats
+    links = []
+    user_name = []
+    group_name = []
+    file_size = []
+    last_update_time = []
+
+    long_formats.each do |long_format|
+      links << long_format[:links]
+      user_name << long_format[:user_name]
+      group_name << long_format[:group_name]
+      file_size << long_format[:file_size]
+      last_update_time << long_format[:last_update_time]
+    end
+
+    {
+      links: get_maximum_length(links),
+      user_name: get_maximum_length(user_name),
+      group_name: get_maximum_length(group_name),
+      file_size: get_maximum_length(file_size),
+      last_update_time: get_maximum_length(last_update_time)
+    }
+  end
+
+  def get_maximum_length(value)
+    value.map(&:length).max
+  end
+
+  def result_with_l_option
+    max_length = get_max_length(get_long_formats)
+    get_long_formats.each do |long_format|
+      puts "#{long_format[:permission]}  #{long_format[:links].rjust(max_length[:links])} #{long_format[:user_name].ljust(max_length[:user_name])}  #{long_format[:group_name].ljust(max_length[:group_name])}  #{long_format[:file_size].rjust(max_length[:file_size])} #{long_format[:last_update_time].rjust(max_length[:last_update_time])} #{long_format[:file_name].ljust(MAX_NUMBER_OF_CHARACTER)} "
+    end
+  end
+
+  def get_mtime(stat)
+    if (Time.now - stat.mtime) / (60 * 60 * 24) >= (365 / 2) || (Time.now - stat.mtime).negative?
+      stat.mtime.strftime('%_m %_d  %Y')
+    else
+      stat.mtime.strftime('%_m %_d %H:%M')
     end
   end
 
@@ -62,9 +111,9 @@ class ListBuilder
     # [0..1]によってファイルかディレクトリを判定
     case overhaul_permission[0..1].join
     when '10'
-      permission_conversioned.prepend('--')
+      permission_conversioned.prepend('-')
     when '40'
-      permission_conversioned.prepend('d-')
+      permission_conversioned.prepend('d')
     end
     permission_conversioned.join
   end
