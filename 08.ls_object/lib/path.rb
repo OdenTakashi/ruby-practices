f1047f31c2fabb42594e766cf90891d1a46f9188
OdenTakashi <odentakashi@icloud.com>
2022-09-15 21:57:59 +0900

    変数の順番を修正

--- 08.ls_object/lib/path.rb
+++ 08.ls_object/lib/path.rb
@@ -13,16 +13,24 @@ class Path
     expand_path = File.expand_path(@name, parent_directory)
     stat = File::Stat.new(expand_path)
 
+    @type = judge_type(stat)
+    @permission = convert_permission(stat)
     @links = stat.nlink
     @user_name = Etc.getpwuid(stat.uid).name
     @group_name = Etc.getgrgid(stat.gid).name
     @file_size = File.size(expand_path)
     @last_update_time = get_mtime(stat)
-    @type = judge_type(stat)
-    @permission = convert_permission(stat)
   end
 
   private
+
+  def get_mtime(stat)
+    if (Time.now - stat.mtime) / (60 * 60 * 24) >= (365 / 2) || (Time.now - stat.mtime).negative?
+      stat.mtime.strftime('%_m %_d  %Y')
+    else
+      stat.mtime.strftime('%_m %_d %H:%M')
+    end
+  end
 
   def judge_type(stat)
     stat.directory? ? 'd' : '-'
@@ -31,14 +39,6 @@ class Path
   def convert_permission(stat)
     permission = stat.mode.to_s(8)[-3..-1].split('')
     permission.map { |n| PERMISSION_TABLE[n] }.join
-  end
-
-  def get_mtime(stat)
-    if (Time.now - stat.mtime) / (60 * 60 * 24) >= (365 / 2) || (Time.now - stat.mtime).negative?
-      stat.mtime.strftime('%_m %_d  %Y')
-    else
-      stat.mtime.strftime('%_m %_d %H:%M')
-    end
   end
 end
 
