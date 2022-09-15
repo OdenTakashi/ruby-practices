ae0ac786acff27c996aa5596c9c3d03a7ede915f
OdenTakashi <odentakashi@icloud.com>
2022-09-15 22:15:25 +0900

    不要なto_sを削除

--- 08.ls_object/lib/path_list_formatter.rb
+++ 08.ls_object/lib/path_list_formatter.rb
@@ -69,13 +69,13 @@ class PathListFormatter
     max_length = gather_max_length
     @path_list.list.each do |path|
       print(
-        path.type.to_s,
-        path.permission.to_s,
-        path.links.to_s.rjust(max_length[:links] + 1).to_s,
-        path.user_name.rjust(max_length[:user_name] + 1).to_s,
-        path.group_name.rjust(max_length[:group_name] + 1).to_s,
+        path.type,
+        path.permission,
+        path.links.to_s.rjust(max_length[:links] + 1),
+        path.user_name.rjust(max_length[:user_name] + 1),
+        path.group_name.rjust(max_length[:group_name] + 1),
         path.file_size.to_s.rjust(max_length[:file_size] + 1).to_s,
-        path.last_update_time.rjust(max_length[:last_update_time] + 1).to_s,
+        path.last_update_time.rjust(max_length[:last_update_time] + 1),
         " #{path.name.ljust(MAX_NUMBER_OF_CHARACTER)}\n"
       )
     end
